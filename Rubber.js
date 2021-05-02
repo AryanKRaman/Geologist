8b2048dba5dc736ae36a2bd7535a6256dc40de65
AryanKRaman <75371016+AryanKRaman@users.noreply.github.com>
2021-05-02 11:59:38 +0530

    Add files via upload
    First

---
+++ Rubber.js
@@ -0,0 +1,25 @@
+class Rubber{
+    constructor(x,y,radius){
+     var options={
+      restitution:0.3,
+      friction:5,
+      density:1
+
+     }
+     this.body = Bodies.circle(x, y, radius-20/2, options);
+     this.radius = radius;
+     World.add(world, this.body);
+    }
+    display(){
+        var angle = this.body.angle;
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        imageMode(CENTER);
+        strokeWeight(3);
+        stroke("green");
+        fill("red");
+        pop();
+
+    }
+}
